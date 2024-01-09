0c5790e3f66f3f6d121fb936deb5b4daebdb7c1a
DiskominfoWsb <94028679+DiskominfoWsb@users.noreply.github.com>
2024-01-09 15:19:25 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -49,7 +49,9 @@ class AppServiceProvider extends ServiceProvider
 
         // }
 
-
+        if (env('APP_ENV') != 'local') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
 
     }
 }
